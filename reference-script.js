82b36cd5d62f6eea48202fa1529490567b9b0e91
ehly <94268602+hellersqueen@users.noreply.github.com>
2023-09-08 19:30:18 -0400

    Create reference-script.js

---
+++ reference-script.js
@@ -0,0 +1,22 @@
+// reference-script.js
+
+// Define variables for element IDs or selectors
+const tagButtonSelector = '.custom-tag-button';
+const customTagInputSelector = '.custom-tag-input';
+const inputFieldSelector = '.custom-tag-input input';
+const postFooterSelector = '.post-form--footer';
+const tagEditorSelector = '.post-form--tag-editor';
+const postContainerSelector = '.post-container';
+const tagInputWrapperSelector = '.tag-input-wrapper';
+const tagInputSelector = '.editor-plaintext';
+const tagLabelsSelector = '.tag-label';
+const customPopoverSelector = '.custom-popover';
+const customPopoverHeaderSelector = '.custom-popover-header';
+const customPopoverTagsSelector = '.custom-popover-tag button';
+
+// New variables from the provided script
+const POST_CONTAINER_TAGS_INPUT_SELECTOR = '.editor-plaintext';
+const POST_CONTAINER_TAGS_INPUT_WRAPPER_SELECTOR = '.post-form--tag-editor';
+const TAG_LABEL_SELECTOR = '.tag-label';
+
+// Add more variables as needed
